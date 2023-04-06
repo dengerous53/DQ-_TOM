a9ab4e05bec0c10b0e9b262faad29d257d718664
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-06 20:53:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1508,13 +1508,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
-            text="▣ ▢ ▢"
+            text="● ◌ ◌"
         )
         await query.message.edit_text(
-            text="▣ ▣ ▢"
+            text="● ● ◌"
         )
         await query.message.edit_text(
-            text="▣ ▣ ▣"
+            text="● ● ●"
         )
         await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
