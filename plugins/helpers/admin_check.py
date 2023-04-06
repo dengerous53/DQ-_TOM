4d8fd8585c50363843c628a6d0b001d0cbd4d080
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-06 19:53:24 +0530

    Update admin_check.py

--- plugins/helpers/admin_check.py
+++ plugins/helpers/admin_check.py
@@ -1,12 +1,12 @@
 from pyrogram.types import Message
-from pyrogram import enums
+from pyrogram import filters, enums 
 
 
 async def admin_check(message: Message) -> bool:
     if not message.from_user:
         return False
 
-    if message.chat.type not in [enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL]:
+    if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         return False
 
     if message.from_user.id in [
@@ -23,12 +23,14 @@ async def admin_check(message: Message) -> bool:
         chat_id=chat_id,
         user_id=user_id
     )
-    admin_strings = [
-        enums.ChatMemberStatus.OWNER,
-        enums.ChatMemberStatus.ADMINISTRATOR
-    ]
+    admin_strings = [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]
     # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
     if check_status.status not in admin_strings:
         return False
     else:
         return True
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+admin_fliter = filters.create(func=admin_filter_f, name="AdminFilter")
