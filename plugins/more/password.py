f4f2c09b3c61845620686c0c5a4b3bf129f8b255
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-06 22:35:48 +0530

    Update password.py

--- plugins/more/password.py
+++ plugins/more/password.py
@@ -12,4 +12,4 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('⚡️ʙᴏᴛ ᴜᴩᴅᴀᴛᴇꜱ⚡️', url='https://t.me/Hs_Botz')
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('⚡️ʙᴏᴛ ᴜᴩᴅᴀᴛᴇꜱ⚡️', url='https://t.me/Hs_Botz']])
