95914b582d9afe144cd7c04bffea4c82337c4206
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 17:17:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -66,7 +66,7 @@ async def give_filter(client, message):
     userid = message.reply_to_message.from_user.id
     content = message.text
     if int(userid) not in [message.from_user.id, 0]:
-        return await query.answer("This Is Not For You!", show_alert=True)                              
+        return await message.reply_text("This Is Not For You!")                              
     if LOGIN_CHANNEL and not await mute_login(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(LOGIN_CHANNEL))          
