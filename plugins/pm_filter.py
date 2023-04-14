44a3efd664dc4c4880e8b1078d148268e8a34ac4
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 21:39:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ SPELL_CHECK = {}
 
 
 
-@Client.on_message(filters.forwarded | filters.group & filters.media)
+@Client.on_message(filters.group & filters.media)
 async def invite(client, message):
     chat_id = message.chat.id
     reporter = str(message.from_user.id)
