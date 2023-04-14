67254420f9d882dcad38a90f4758a5c79b53c287
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 19:54:48 +0530

    Update ForwardTag.py

--- plugins/security/ForwardTag.py
+++ plugins/security/ForwardTag.py
@@ -2,13 +2,16 @@ import pyrogram
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
-
+import asyncio
 
 @Client.on_message(filters.forwarded & filters.group & filters.incoming)
 async def channel_tag(bot, message):
     try:
         chat_id = message.chat.id
         forward_msg = await message.copy(chat_id)
+        await message.delete()
+        await asyncio.sleep(5)
+        await forward_msg.delete()
         await message.delete()
     except:
         await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
