3b3badd8aa13da2edc8fccb4cc4451237308b8c3
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 19:57:38 +0530

    Create NoLink.py

---
+++ plugins/security/NoLink.py
@@ -0,0 +1,17 @@
+import pyrogram
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+import asyncio
+
+@Client.on_message((filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
+async def channel_tag(bot, message):
+    try:
+        chat_id = message.chat.id
+        forward_msg = await message.copy(chat_id)
+        await message.delete()
+        await asyncio.sleep(1)
+        await forward_msg.delete()
+        
+    except:
+        await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
