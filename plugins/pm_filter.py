af8ec3fdbe0eda07b003fe267eee3290ced06f03
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 17:33:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -48,7 +48,7 @@ async def invite(client, message):
     buttons = [[
         InlineKeyboardButton("📢 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 📢", url=invite_link.invite_link)
     ],[
-        InlineKeyboardButton("🔁 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data="grp_checksub")
+        InlineKeyboardButton("🔁 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data="check_delete")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
@@ -69,7 +69,7 @@ async def invite(client, message):
     buttons = [[
         InlineKeyboardButton("📢 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 📢", url=invite_link.invite_link)
     ],[
-        InlineKeyboardButton("🔁 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data="grp_checksub")
+        InlineKeyboardButton("🔁 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data="check_delete")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
@@ -1000,6 +1000,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return
         await client.unban_chat_member(query.message.chat.id, query.from_user.id)
         await query.answer("Can You Request Now!", show_alert=True)
+        await query.message.delete()
+        await query.message.reply_to_message.delete()
+
+
+    elif query.data.startswith("check_delete"):
+        userid = query.message.reply_to_message.from_user.id
+        if int(userid) not in [query.from_user.id, 0]:         
+            await query.answer("Please join first my Updates Channel", show_alert=True)
+            return                
         await query.message.delete()
         await query.message.reply_to_message.delete()
 
