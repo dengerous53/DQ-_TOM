0cb2cc07594f0c1f76a5e0458b0801226b3f53f8
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-14 20:02:30 +0530

    Update NoLink.py

--- plugins/security/NoLink.py
+++ plugins/security/NoLink.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User
 import asyncio
 
-@Client.on_message((filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$") & filters.text ) & filters.private & filters.incoming)
+@Client.on_message((filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.group & filters.incoming)
 async def channel_tag(bot, message):
     try:
         chat_id = message.chat.id
