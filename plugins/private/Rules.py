c33b02bb27cdab56d774972915780e33b892ffa0
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-15 18:06:45 +0530

    Update Rules.py

--- plugins/private/Rules.py
+++ plugins/private/Rules.py
@@ -120,42 +120,8 @@ async def pm_text(bot, message):
 
     
 
-    if imdb:
 
-        cap = BR_IMDB_TEMPLATE.format(
-        query=content,
-        title=imdb['title'],
-        votes=imdb['votes'],
-        aka=imdb["aka"],
-        seasons=imdb["seasons"],
-        box_office=imdb['box_office'],
-        localized_title=imdb['localized_title'],
-        kind=imdb['kind'],
-        imdb_id=imdb["imdb_id"],
-        cast=imdb["cast"],
-        runtime=imdb["runtime"],
-        countries=imdb["countries"],
-        certificates=imdb["certificates"],
-        languages=imdb["languages"],
-        director=imdb["director"],
-        writer=imdb["writer"],
-        producer=imdb["producer"],
-        composer=imdb["composer"],
-        cinematographer=imdb["cinematographer"],
-        music_team=imdb["music_team"],
-        distributors=imdb["distributors"],
-        release_date=imdb['release_date'],
-        year=imdb['year'],
-        genres=imdb['genres'],
-        poster=imdb['poster'],
-        plot=imdb['plot'],
-        rating=imdb['rating'],
-        url=imdb['url'],
-        **locals()
-
-    )                           
-    if imdb and imdb.get('poster'):
-        try:
+    try:
             buttons = [[
                 InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/nasrani_update'),
                 InlineKeyboardButton("𝐒𝐮𝐫𝐩𝐫𝐢𝐬𝐞", url=f"https://telegram.me/{temp.U_NAME}?start"),
@@ -167,7 +133,7 @@ async def pm_text(bot, message):
             parse_mode=enums.ParseMode.HTML
             )
                                       
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+    except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             buttons = [[
@@ -179,8 +145,8 @@ async def pm_text(bot, message):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
             )
-        except Exception as e:
-            logger.exception(e)
+    except Exception as e:
+        logger.exception(e)
 
 
 
