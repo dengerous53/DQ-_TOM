ef8f5dc9baf71ee186f97e207644e1d06260db07
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-16 01:47:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2990,9 +2990,9 @@ async def advantage_spell_chok(client, msg):
         await k.delete()
         return
     content = msg.text
-#    movielist += [f"📥movie.get('title')📥" for movie in movies]
+    movielist += [📥 movie.get('title') for movie in movies]
 #    movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
-    movielist += [f📥 "{movie.get('title')} 📥" for movie in movies]
+    movielist += [f"📥 "{movie.get('title')} 📥" for movie in movies]
     imdb = await get_poster(content) if IMDB else None
     SPELL_CHECK[mv_id] = movielist
     i = 1
