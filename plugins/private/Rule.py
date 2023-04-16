6519eaeb876e19d602ad1b3615a88c0740900746
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-16 16:48:42 +0530

    Update Rule.py

--- plugins/private/Rule.py
+++ plugins/private/Rule.py
@@ -66,7 +66,7 @@ START_MESSAGE = """
 
 
 
-@Client.on_message(filters.command("rules") & filters.group & filters.media)
+@Client.on_message(filters.command("rules") & filters.media)
 async def media(client, message):
     userid = message.from_user.id            
     buttons = [[
