091d149efdc55c24437d2d921901afb919041bcb
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-16 12:47:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 #redict
 MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/nasrani_update")
-FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+A9emw13aEyxkYzE1")
+FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/bigmoviesworld")
 MSG_ALRT = environ.get('MSG_ALRT', '𝑪𝑯𝑬𝑪𝑲 & 𝑻𝑹𝒀 𝑨𝑳𝑳 𝑴𝒀 𝑭𝑬𝑨𝑻𝑼𝑹𝑬𝑺')
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
 
