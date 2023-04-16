d06ca3d123ab06afa45418839a06c4bf98126f04
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-16 15:23:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -623,11 +623,11 @@ async def send_all(bot, userid, files, ident):
         f_caption = file.caption
         title = file.file_name
         size = get_size(file.file_size)
-        if CUSTOM_FILE_CAPTION:
+        if CAPTION:
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                        file_size='' if size is None else size,
-                                                        file_caption='' if f_caption is None else f_caption)
+                f_caption = CAPTION.format(file_name='' if title is None else title,
+                                            file_size='' if size is None else size,
+                                            file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 print(e)
                 f_caption = f_caption
