030c1fb51bc846e1a5bc22397e75d9aae24e2bd4
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-18 16:25:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,7 +57,7 @@ async def start(client, message):
             InlineKeyboardButton('𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩', url='https://t.me/nasrani_update')
         ], [
             InlineKeyboardButton('𝐈𝐧𝐥𝐢𝐧𝐞', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('𝐒𝐞𝐭𝐭𝐢𝐧𝐠𝐬', callback_data=f"opnsetpm#{grp_id}")
+            InlineKeyboardButton('𝐒𝐞𝐭𝐭𝐢𝐧𝐠𝐬', callback_data=f"opnsetpm")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
