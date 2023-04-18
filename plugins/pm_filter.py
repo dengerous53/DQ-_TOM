ee6a14ba6c5bf7f741c139ac8eeb08b83f5a96fb
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-18 14:55:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1757,7 +1757,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
-            text=script.STICKER,
+            text=script.STICKER.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
