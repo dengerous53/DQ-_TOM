9620feb7bed79029b11b7b3a4319b066a234393f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-18 15:08:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1758,7 +1758,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.STICKER,
+            text=script.STICKER.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
